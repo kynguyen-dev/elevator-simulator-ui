bcd743f80a8e9ed6a1557b5df539203c26e8ebc6
kynguyen-dev <libra101099k@gmail.com>
2025-03-18 11:11:23 +0700

    feat: update env variable name

--- src/services/elevator.service.ts
+++ src/services/elevator.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Elevator } from "../models/elevator.ts";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 /**
  * Fetch the state of a specific elevator.
